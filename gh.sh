da3b79a5f37c1fefe66a76d557b244b000664a2a
gnostis <saridakisbitos@gmail.com>
2021-01-16 17:01:33 +0200

    I am an automated comment

--- gh.sh
+++ gh.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-#fuction gh {
-#  git add .
-#  git commit -m 'I am an automated comment'
-#  git push
-#}
+gh () {
+  git add .
+  git commit -m 'I am an automated comment'
+  git push
+}
 
-git add .
-git commit -m 'I am an automated comment'
-git push
-cd ../2StunningHelpFiles
-git add .
-git commit -m 'I am an automated comment'
-git push
-cd ../3StunningExecutables
-git add .
-git commit -m 'I am an automated comment'
-git push
-cd ../4TheRest
-git add .
-git commit -m 'I am an automated comment'
-git push
+gh
+cd ../StunningHelpFiles
+gh
+cd ../StunningExecutables
+gh
+cd ../Stuff
+gh
