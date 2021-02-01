6df8b3931bdfbb86835c3c6089b036efa8aaf009
gnostis <saridakisbitos@gmail.com>
2021-02-01 16:08:51 +0200

    I am an automated comment

--- setUpLinks.sh
+++ setUpLinks.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 configs="$HOME/.config"
+
 configBackupsPath="$HOME/.configBackups"
+
 stunningConfigs="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 script="$stunningConfigs/dirFromFile.sh"
+
 mkdir $configBackupsPath
+
 for file in {.vimrc,.bashrc,.bash_aliases,.Xresources,.urxvt}
 do
   filePath="$HOME/$file"
+
   mv $filePath $configBackupsPath
+
   intermiateDir=$($script $file)
-  ln -s $stunningConfigs/$intermiateDir/$file $HOME
+
+ln -s $stunningConfigs/$intermiateDir/$file $HOME
 done
+
 for dir in {i3blocks,i3,ranger}
 do
   mv $configs/$dir $configBackupsPath
   ln -s $stunningConfigs/$dir $configs/$dir
 done
+
 source "$HOME/.bashrc"
